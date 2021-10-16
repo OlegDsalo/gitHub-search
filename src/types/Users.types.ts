411cd85f02c94d803d7aadfd0a47079872616980
OlegDsalo <87490184+OlegDsalo@users.noreply.github.com>
2021-10-16 16:17:23 +0300

    add types

---
+++ src/types/Users.types.ts
@@ -0,0 +1,12 @@
+export interface UsersTypes{
+ login:string;
+ avatar_url:string;
+ id:string;
+}
+
+export interface UserTypes{
+    name:string;
+    html_url:string;
+    forks:string;
+    stargazers_count:string;
+}
