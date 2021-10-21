ef90c6766c3f48ca1be318894c0b32c4ba5380a6
OlegDsalo <oleg.d.salo@gmail.com>
2021-10-21 22:06:39 +0300

    add types files

--- src/types/Users.types.ts
+++ src/types/Users.types.ts
@@ -1,12 +1,5 @@
 export interface UsersTypes {
     login: string;
     avatar_url: string;
-    id: string;
-}
-
-export interface UserReposType {
-    name: string;
-    html_url: string;
-    forks: string;
-    stargazers_count: string;
+    id: number;
 }
